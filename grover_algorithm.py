56f29f2e07a0108d121a92495b9e8d3ecbfe0bc0
nikhila2007-ux <nikhilaprashanth24b1242@gmail.com>
2025-06-24 13:05:31 +0530

    Create grover_algorithm.py

---
+++ grover_algorithm.py
@@ -0,0 +1,10 @@
+from qiskit import QuantumCircuit, Aer, execute
+qc = QuantumCircuit(2,2)
+qc.h([0,1])
+qc.cz(0,1)
+qc.h([0,1])
+qc.measure([0,1], [0,1])
+simulator = Aer.get_backend('qasm_simulator')
+result = execute(qc, simulator, shots=1024).result()
+counts = result.get_counts()
+print(counts)
