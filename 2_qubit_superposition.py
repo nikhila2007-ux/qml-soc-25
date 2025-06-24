fe1785e648d1e7fc4d0fbe7529bccde6d01f3d8f
nikhila2007-ux <nikhilaprashanth24b1242@gmail.com>
2025-06-24 13:00:02 +0530

    Create 2_qubit_superposition.py

---
+++ 2_qubit_superposition.py
@@ -0,0 +1,9 @@
+from qiskit import QuantumCircuit, Aer, execute
+qc = QuantumCircuit(2, 2)
+qc.h(0)
+qc.h(1)
+qc.measure([0, 1], [0, 1])
+simulator = Aer.get_backend('qasm_simulator')
+result = execute(qc, simulator, shots=1024).result()
+counts = result.get_counts()
+print(counts)
